320a2f346662354aea501b32c8da3647b86f519e
EmanuelMiron <emanuel.miron6@gmail.com>
2025-07-21 16:01:46 +0300

    testing shadcn wrapped components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import ShadcnHome from "./pages/ShadcnHome";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Routes>
           <Route index element={<Home />} />
+          <Route path="shadcn" element={<ShadcnHome />} />
           <Route path="*" element={<NoMatch />} />
       </Routes>
     </ThemeProvider>
