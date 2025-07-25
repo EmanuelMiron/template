fa6cc44c23e1f20a3b1a3e7b30d466b38cff66c8
EmanuelMiron <emanuel.miron6@gmail.com>
2025-07-25 14:49:15 +0300

    Clean Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import Test from "./pages/Test";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Routes>
           <Route index element={<Home />} />
+          <Route path="/test" element={<Test />} />
           <Route path="*" element={<NoMatch />} />
       </Routes>
     </ThemeProvider>
